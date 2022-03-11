9af8732861036f64015a9012fd1554b26dd213a8
mak448a <mak448a@gmail.com>
2022-03-10 20:14:41 -0600

    Added sky image and added pygame.display.update() to the main function

--- HirokiPoops.py
+++ HirokiPoops.py
@@ -1,11 +1,14 @@
 import sys
 import pygame
-from utils import window
+from utils.screen import window
+from utils.load_image import load_image
 
 pygame.init()
 
 
 clock = pygame.time.Clock()
+sky_image = load_image("img/sky.png").convert()
+
 
 # Add user events so that we can add poops on a timer
 add_poop = pygame.USEREVENT + 1
@@ -14,15 +17,17 @@ add_cloud = pygame.USEREVENT + 2
 pygame.time.set_timer(add_cloud, 1000)
 
 
-def main():
+def main(win):
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
 
-        window.fill("black")
+        win.blit(sky_image, (0, 0))
+
+        pygame.display.update()
 
 
 if __name__ == '__main__':
-    main()
+    main(window)
