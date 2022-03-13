b8f4c655394863f03a62c87f14b83117441dad75
mak448a <mak448a@gmail.com>
2022-03-12 21:38:27 -0600

    Added game icon to the window

--- utils/screen.py
+++ utils/screen.py
@@ -1,6 +1,8 @@
 import pygame
+import os
 
 pygame.init()
 
 width, height = 800, 600
 window = pygame.display.set_mode((width, height), pygame.SCALED)
+pygame.display.set_icon(pygame.image.load(os.path.join(os.getcwd(), "img/game_icon.png")))
