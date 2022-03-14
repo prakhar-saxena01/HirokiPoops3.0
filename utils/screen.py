60cba13ac922eaf8a460440aec6f5d60819104f7
mak448a <mak448a@gmail.com>
2022-03-13 22:27:15 -0500

    Optimized loading game icon

--- utils/screen.py
+++ utils/screen.py
@@ -5,4 +5,4 @@ pygame.init()
 
 width, height = 800, 600
 window = pygame.display.set_mode((width, height), pygame.SCALED)
-pygame.display.set_icon(pygame.image.load(os.path.join(os.getcwd(), "img/game_icon.png")))
+pygame.display.set_icon(pygame.image.load(os.path.join(os.getcwd(), "img/game_icon.png")).convert_alpha())
