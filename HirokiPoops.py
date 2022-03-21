36408290a68417077e2eff0f01e967ce73262bcc
mak448a <mak448a@gmail.com>
2022-03-21 18:15:26 -0500

    Fixed bug where music would stop after crashing

--- HirokiPoops.py
+++ HirokiPoops.py
@@ -15,5 +15,7 @@ while g.running:
     g.current_menu.display_menu()
     if g.playing:
         g.game_loop()
+        pygame.mixer.music.load(f"{os.getcwd()}/sounds/he'll_take_care_of_the_rest.mp3")
+        pygame.mixer.music.play(loops=-1)
 
 print("Quit")
